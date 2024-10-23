f23ef8bcd497cbbe2ac0bae8d64971419c9006dc
Cameron Campos <cameroncampos95@gmail.com>
2024-10-23 16:03:51 -0600

    Ship works

--- Godot/projects/space-rocks/player/player.gd
+++ Godot/projects/space-rocks/player/player.gd
@@ -3,7 +3,8 @@ extends RigidBody2D
 @export var engine_power = 500
 @export var spin_power = 8000
 
-var thrust = Vector2.ZERO
+var screensize := Vector2.ZERO
+var thrust := Vector2.ZERO
 var rotation_dir = 0
 
 # Possible states
@@ -11,6 +12,7 @@ enum {INIT, ALIVE, INVULNERABLE, DEAD}
 var state = INIT
 
 func _ready() -> void:
+	screensize = get_viewport_rect().size
 	change_state(ALIVE)
 
 func change_state(new_state):
@@ -24,3 +26,27 @@ func change_state(new_state):
 		DEAD:
 			$CollisionShape2D.set_deferred("disabled", true)
 	state = new_state
+
+func _process(delta: float) -> void:
+	get_input()
+	
+func get_input():
+	thrust = Vector2.ZERO
+	# If dead or none existant, leave
+	if state in [DEAD,INIT]:
+		return
+	if Input.is_action_pressed("thrust"):
+		thrust = transform.x * engine_power
+	rotation_dir = Input.get_axis("rotate_left", "rotate_right")
+	
+func _physics_process(delta: float) -> void:
+	constant_force = thrust
+	constant_torque = rotation_dir * spin_power
+
+# Screen Wraping
+func _integrate_forces(physics_state: PhysicsDirectBodyState2D) -> void:
+	var xform = physics_state.transform
+	# wrapf is like a vector clamp but for whatever the first argument is.
+	xform.origin.x = wrapf(xform.origin.x, 0, screensize.x)
+	xform.origin.y = wrapf(xform.origin.y, 0, screensize.y)
+	physics_state.transform = xform
